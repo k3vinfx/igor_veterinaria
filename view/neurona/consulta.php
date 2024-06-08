105f6fc57dafebee20075c1447f2a63e41dd7c91
Kevin Carrillo <k3vinfx@gmail.com>
2024-06-08 02:26:25 -0400

    Fix nueva neurona consulta X3

--- view/neurona/consulta.php
+++ view/neurona/consulta.php
@@ -261,7 +261,7 @@ dialog {
                                         data-tratamiento2="<?php echo $r->enfermedadTratamiento2; ?>"      
                                         data-estado="<?php echo $r->estado; ?>"
 
-                                        data-toggle="modal" data-target="#RegistroMVC_IA">
+                                        data-toggle="modal" data-target="#RegistroMVC">
                                          >                                  
                                   <i class='fas fa-edit'></i>Registrar</button> <?php }
                                   else{
