1a8431588dfedbc3291af56e7602fd3f7940e52a
Kevin Carrillo <k3vinfx@gmail.com>
2024-08-18 03:32:51 -0400

    RRN PARTE 3

--- view/neurona/consulta.php
+++ view/neurona/consulta.php
@@ -85,7 +85,7 @@ dialog {
                                     <div class="modal-dialog modal-lg" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Nueva Consulta</h5>
+                                                <h5 class="modal-title" id="exampleModalLabel">Nueva Consulta....</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
