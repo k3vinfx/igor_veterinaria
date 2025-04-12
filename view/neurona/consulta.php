c479ec3247c6c6a81a77eb5c7ff1c4bb7e011405
Kevin Carrillo <k3vinfx@gmail.com>
2025-04-12 19:31:06 -0400

    dosificacion

--- view/neurona/consulta.php
+++ view/neurona/consulta.php
@@ -80,7 +80,7 @@ dialog {
                                     <?php  if ($r->estado==0){echo "Inactivo";?> 
 
                             <!-- Modal para Registro de Propietario -->
-                            <div class="modal fade" id="RegistroMVC" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="RegistroMVC" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
    
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
--- view/neurona/dosificacion.php
+++ view/neurona/dosificacion.php
@@ -122,18 +122,26 @@
                                 <label for="duracion">Duración</label>
                                 <input type="text" class="form-control" id="duracion" placeholder="Ej: 7 días" required>
                             </div>
-                            
+
+                            <div class="form-group">
+                                <label for="medicamento">Medicamento</label>
+                                <input type="text" class="form-control" rows="2" id="medicamento" placeholder="Ej: Iboprefeno" required>
+                            </div>
+                          
+                         
+                        </div>
+                        <div class="col-md-6">
                             <div class="form-group">
                                 <label for="fechaInicio">Fecha de Inicio</label>
                                 <input type="date" class="form-control" id="fechaInicio" required>
                             </div>
+                            <div class="form-group">
+                                <label for="fechaFinal">Fecha de Final</label>
+                                <input type="date" class="form-control" id="fechaFinal" required>
+                            </div>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="medicamento">Medicamento</label>
-                        <input type="text" class="form-control" rows="2" id="medicamento" placeholder="Ej: Iboprefeno" required>
-                    </div>
-                    
+                           
                     
                     <div class="form-group">
                         <label for="observaciones">Observaciones</label>
