124a336739a35bc516b9af381b270de7d10d3e2b
Kevin Carrillo <k3vinfx@gmail.com>
2025-04-12 11:53:16 -0400

    dosificacion

--- view/neurona/dosificacion.php
+++ view/neurona/dosificacion.php
@@ -42,7 +42,7 @@ dialog {
 
 <div class="container-fluid">
     <div class="d-sm-flex align-items-center justify-content-between mb-6">
-        <h1 class="h3 mb-0 text-gray-800">Dosificacion.</h1>
+        <h1 class="h3 mb-0 text-gray-800">Dosificación.</h1>
         
     </div>
 
